038d41150fcaa0018106a02a0fe9c6333d1b4965
chains-ah <95008270+chains-ah@users.noreply.github.com>
2024-07-17 10:01:55 +0800

    Update site.config.ts
    描述修改

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My Blog.',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
